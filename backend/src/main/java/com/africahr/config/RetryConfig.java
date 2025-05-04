1fc0c3b537f132326eceeb532a5aff945ed2e659
alain-x <alainvalentin04@gmail.com>
2025-05-04 14:19:04 +0200

    initial commi

---
+++ backend/src/main/java/com/africahr/config/RetryConfig.java
@@ -0,0 +1,27 @@
+package com.africahr.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class RetryConfig {
+    
+    @Bean
+    public DataSource dataSource(
+            @Value("${spring.datasource.url}") String url,
+            @Value("${spring.datasource.username}") String username,
+            @Value("${spring.datasource.password}") String password) {
+        
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        dataSource.setConnectionTimeout(30000); // 30 seconds
+        dataSource.setInitializationFailTimeout(60000); // 60 seconds
+        return dataSource;
+    }
+}
